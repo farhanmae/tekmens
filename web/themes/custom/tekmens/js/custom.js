790190879f3d9c75e48800c039409ad78510316b
Farhan Erooth <farhanmae@gmail.com>
2023-11-19 11:36:01 +0530

    Logo animation added

--- web/themes/custom/tekmens/js/custom.js
+++ web/themes/custom/tekmens/js/custom.js
@@ -2,10 +2,20 @@
     // Make header sticky on scroll
     $(window).scroll(function() {
         var sticky = $('header#header-brand'),
+            lock = $('#logotop'),
             scroll = $(window).scrollTop();
 
         if (scroll >= 120) sticky.addClass('fixed');
         else sticky.removeClass('fixed');
+
+        if (scroll >= 120 && scroll <= 239) lock.css('top',10);
+        if (scroll >= 240 && scroll <= 359) lock.css('top',8);
+        if (scroll >= 360 && scroll <= 479) lock.css('top',6);
+        if (scroll >= 480 && scroll <= 599) lock.css('top',4);
+        if (scroll >= 600) lock.css('top',2);
+
+
     });
+
     
 })(jQuery, Drupal);
